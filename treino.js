bd8a09541fe083a1be1dc99cbd896a0d95c12b3e
Gustavo Luiz <70780066+luiz-lgrp@users.noreply.github.com>
2021-10-31 19:57:08 -0300

    Primeiro commit

---
+++ treino.js
@@ -0,0 +1,26 @@
+//VARIAVEIS
+
+// Declarar uma variável com o seu nome
+
+var nome = "Luiz";
+
+// Declarar uma variável com a sua idade
+
+var idade = 30;
+
+// Declarar uma variável com a sua comida favorita e não atribuir valor
+
+var comidaFavorita;
+
+// Atribuir valor a sua comida favorita
+
+var comidaFavorita = "Massas"
+
+// Declarar 5 variáveis diferentes sem valores
+
+var um,
+dois,
+tres,
+quatro,
+cinco
+
---
+++ treino1.js
@@ -0,0 +1,32 @@
+
+// 7 TIPOS DE DADOS
+
+// Declare uma variável contendo uma string
+
+var cao = "caramelo"
+
+// Declare uma variável contendo um número dentro de uma string
+
+var letra = "half";
+var numero = 28;
+
+var misto = letra + numero
+
+// Declare uma variável com a sua idade
+
+var idade2 = 30;
+
+// Declare duas variáveis, uma com seu nome e outra com seu sobrenome e some as mesmas
+
+var meuNome = "Luiz"
+var meuSobrenome = "Gustavo"
+
+var nomeCompleto = meuNome + " " + meuSobrenome
+
+// Coloque a seguinte frase em uma variável: It's time
+
+var frase = "It's time"
+
+// Verifique o tipo da variável que contém o seu nome
+
+console.log(typeof nomeCompleto)
